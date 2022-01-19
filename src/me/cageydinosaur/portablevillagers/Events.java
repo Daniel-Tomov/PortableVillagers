a0f3cada5488354d3b6efe3032f6c111f5ad538a
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-01-19 16:11:55 -0500

    Fix bugs and build errors

--- src/me/cageydinosaur/portablevillagers/Events.java
+++ src/me/cageydinosaur/portablevillagers/Events.java
@@ -2,8 +2,10 @@ package me.cageydinosaur.portablevillagers;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -18,10 +20,11 @@ public class Events implements Listener {
 
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent e) {
-		if (e.getEntity() instanceof LivingEntity) {
+		if (e.getEntity() instanceof LivingEntity && e.getEntity().getKiller() instanceof Player) {
 			LivingEntity entity = e.getEntity();
+			Ageable age = (Ageable) e.getEntity();
 			Location loc = entity.getLocation();
-			if (entity.getType() == EntityType.VILLAGER) {
+			if (entity.getType() == EntityType.VILLAGER && age.isAdult()) {
 				entity.getKiller().getWorld().dropItem(loc, new ItemStack(Material.VILLAGER_SPAWN_EGG));
 			} else if (entity.getType() == EntityType.ZOMBIE_VILLAGER) {
 				entity.getKiller().getWorld().dropItem(loc, new ItemStack(Material.ZOMBIE_VILLAGER_SPAWN_EGG));
